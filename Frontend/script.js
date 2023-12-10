3b2818c81fb9bbe2454a5462d919ca1d76d93355
Taiwo Ifeoluwase <taiwoifeoluwa023@gmail.com>
2023-12-11 00:37:34 +0100

    made password toggle

--- Frontend/script.js
+++ Frontend/script.js
@@ -3,7 +3,7 @@ function login() {
     const password = document.getElementById('login-password').value;
 
     // Assuming you have a login API endpoint
-    fetch('http://localhost:8000/', {
+    fetch('http://localhost:8000/auth/login', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -24,7 +24,7 @@ function register() {
     const password = document.getElementById('register-password').value;
 
     // Assuming you have a registration API endpoint
-    fetch('http://localhost:8000/', {
+    fetch('http://localhost:8000/auth/register', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -38,4 +38,19 @@ function register() {
         // Redirect or perform other actions based on the response
     })
     .catch(error => console.error('Error:', error));
+}
+
+function togglePassword(inputId) {
+    const passwordInput = document.getElementById(inputId);
+    const showBtn = document.getElementById('showBtn');
+
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        showBtn.classList.remove('fa-eye');
+        showBtn.classList.add('fa-eye-slash');
+    } else {
+        passwordInput.type = 'password';
+        showBtn.classList.remove('fa-eye-slash');
+        showBtn.classList.add('fa-eye');
+    }
 }
